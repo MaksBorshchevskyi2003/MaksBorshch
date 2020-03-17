3c72bb0fe57e5a740c41a97276f6500a7edff71b
Максим Борщевский <60291750+MaksBorshchevskyi2003@users.noreply.github.com>
2020-03-17 13:36:35 +0200

    Add files via upload

---
+++ my.js.js
@@ -0,0 +1,27 @@
+let money = prompt('Введите месячный бюджет');
+let money = prompt('Введите название магазина');
+
+let mainlist = {
+	budget: money,
+	shopName: name,
+	shopGoods: [],
+	employers; {},
+	open: false
+};
+
+let a = prompt('Какой тип товаров будете продовать ?');
+
+
+mainlist.shopGoods[i] = a;
+ 
+if ((typeof (a))=== 'string'&& ((typeof (a))== nul && a!='' 
+
+{
+console.log ("Good")	
+}
+
+function choosGoods(){
+for (let i=1; i < 4  i++ )(
+a =prompt ( Наш магазин включает в себя: )	
+	
+} 
